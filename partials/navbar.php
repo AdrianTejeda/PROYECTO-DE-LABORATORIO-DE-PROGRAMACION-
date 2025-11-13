1a6c4e788c9cbcbe254eee6b958ea0235710f93d
Adrian Tejeda <adrianjaviertejeda@gmail.com>
2025-11-13 13:09:27 -0300

    Edito menu y agrego boton Nosotros

--- partials/navbar.php
+++ partials/navbar.php
@@ -22,10 +22,11 @@
     </button>
     <div class="collapse navbar-collapse" id="mainNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php#inicio">🏠</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php#inicio"></a></li>
         <li class="nav-item"><a class="nav-link" href="index.php#productos">Productos</a></li>
         <li class="nav-item"><a class="nav-link" href="index.php#servicios">Servicios</a></li>
         <li class="nav-item"><a class="nav-link" href="index.php#presupuesto">Presupuesto</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php#nosotros">Nosotros</a></li>
         <li class="nav-item"><a class="nav-link" href="index.php#contacto">Contacto</a></li>
         <li class="nav-item"><a class="nav-link" href="carrito.php"><i class="bi bi-cart"></i> Carrito <?php $cnt = 0; if (!empty($_SESSION['cart'])) { $cnt = array_sum($_SESSION['cart']); } echo "<span class=\"badge text-bg-danger ms-1\">".$cnt."</span>"; ?></a></li>
         <li class="nav-item"><a class="nav-link" href="login.php"><i class="bi bi-person"></i> Login</a></li>
